e3bb42b1b9150ebcc77171e1bb1650bceb56339e
Pedro <pedrofek20@gmail.com>
2026-04-21 07:29:13 -0300

    Meu primeiro commit

---
+++ teste.py
@@ -0,0 +1,13 @@
+nome=input('Qual é seu nome? ')
+dia=input('Que dia voce nasceu? ')
+mes=input('que mes voce nasceu? ')
+ano=input('que ano voce nasceu? ')
+n1=int(input('escolha um numero: '))
+n2=int(input('escolha outro numero: '))
+resultado=n1+n2
+
+print(f'Boas vindas', {nome})
+print(f'voce nasceu no dia, {dia}/{mes}/{ano}')
+print(f'a soma de {n1} + {n2} é igual a: {resultado}')
+
+#teste
