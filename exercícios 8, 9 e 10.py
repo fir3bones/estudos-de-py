a7b1444d614262684a8b14d524516344940b5969
Pedro <pedrofek20@gmail.com>
2026-04-22 23:38:06 -0300

    8, 9 e 10

---
+++ exercícios 8, 9 e 10.py
@@ -0,0 +1,22 @@
+#Exerc.8
+
+n1=float(input('Metros: '))
+n2=n1*100
+n3=n1*1000
+
+print(f'{n1} metros equivalem a {n2:.0f} centímetros e {n3:.0f} milímetros')
+
+#Exerc.9
+
+p1=int(input('Qual a tabuada de? '))
+t=p1*1, p1*2, p1*3, p1*4, p1*5, p1*6, p1*7, p1*8, p1*9, p1*10
+
+print(f'A tabuada de {p1} é:', t)
+
+#Exerc.10
+
+r1=float(input('Quantos reais Pedro tem? '))
+d=r1/4.99
+
+print('Pedro tem', r1, 'reais')
+print(f'Ele pode comprar {d:.0f} dólares ')
