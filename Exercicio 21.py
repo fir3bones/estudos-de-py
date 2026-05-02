1fe499f95497c37ad58da0a558c4181153ad70ec
Pedro <pedrofek20@gmail.com>
2026-05-01 23:30:01 -0300

    Exercícios 20 e 21

---
+++ Exercicio 21.py
@@ -0,0 +1,6 @@
+import pygame
+
+pygame.init()
+pygame.mixer.music.load('audiomp3.mp3')
+pygame.mixer.music.play()
+input()
---
+++ exercício 20.py
@@ -0,0 +1,9 @@
+import random
+lista=(['pedro','jorge','julia','marcos'])
+random.shuffle(lista)
+
+print('a ordem de apresentação é: ', lista)
+
+
+
+#Penei pra fazer, nao soube fazer corretamente precisei de ensinamento externo a mais, nao sabia que tinha que separar o list do random.shuffle, e descobri que adicionando um join(lista) no print ele tira esses '[]'
